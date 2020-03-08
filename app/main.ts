a982f2b0262b465810858510c71cf0d531a38964
GeoDaoyu <geodaoyu@foxmail.com>
2020-03-08 09:55:00 +0800

    Initial commit

---
+++ app/main.ts
@@ -0,0 +1,15 @@
+import EsriMap from "esri/Map";
+import MapView from "esri/views/MapView";
+
+const map = new EsriMap({
+  basemap: "streets"
+});
+
+const view = new MapView({
+  map: map,
+  container: "viewDiv",
+  center: [-118.244, 34.052],
+  zoom: 12
+});
+
+view.ui.remove("attribution");
