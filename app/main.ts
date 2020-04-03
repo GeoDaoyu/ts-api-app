d819437f79b601c38fad4270a9e78cb5dbe6e8b5
zhanggy <geodaoyu@foxmail.com>
2020-04-03 15:19:59 +0800

    TianDiTuLayer Demo

--- app/main.ts
+++ app/main.ts
@@ -1,33 +1,49 @@
 import EsriMap from "esri/Map";
-import MapView from "esri/views/MapView";
-import TianDiTuLayer from "utils/TianDiTuLayer";
+import SceneView from "esri/views/SceneView";
 import Basemap from "esri/Basemap";
+import TianDiTuLayer from "app/utils/TianDiTuLayer";
 import SpatialReference from "esri/geometry/SpatialReference";
-
-const spatialReference = SpatialReference.WGS84;
-
-// const webTileLayer = new TianDiTuLayer({
-//   urlTemplate: "http://t0.tianditu.com/vec_w/wmts"
-// } as TianDiTuLayer);
-
-const webTileLayer = new TianDiTuLayer({
-  urlTemplate: "http://t0.tianditu.com/img_c/wmts",
-  spatialReference
-} as TianDiTuLayer);
+import domConstruct from "dojo/dom-construct";
+import on from "dojo/on";
 
 const basemap = new Basemap({
-  baseLayers: [webTileLayer]
+  baseLayers: [
+    new TianDiTuLayer({
+      urlTemplate: "http://t0.tianditu.com/vec_w/wmts",
+    } as TianDiTuLayer)
+  ]
 });
 
 const map = new EsriMap({
   basemap
 });
 
-const view = new MapView({
-  map,
+const nextBasemap = new Basemap({
+  baseLayers: [
+    new TianDiTuLayer({
+      urlTemplate: "http://t0.tianditu.com/img_c/wmts",
+      spatialReference: SpatialReference.WGS84
+    } as TianDiTuLayer)
+  ]
+});
+
+const map2 = new EsriMap({
+  basemap: nextBasemap
+});
+
+const view = new SceneView({
+  map: map,
   container: "viewDiv",
   center: [120, 32],
   zoom: 8
+});
+
+const html = `<button id="btn" style="position: absolute; left: 50px; bottom: 50px">切换</button>`;
+domConstruct.place(html, view.container, "last");
+
+const btn = document.getElementById("btn");
+on(btn, "click", () => {
+  view.map = map2;
 });
 
 view.ui.remove("attribution");
