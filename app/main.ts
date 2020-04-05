17dfa55f1e21f8508835d69b20492203650ff15a
GeoDaoyu <GeoDaoyu@foxmail.com>
2020-04-06 00:02:30 +0800

    TianDiTuLayer Demo

--- app/main.ts
+++ app/main.ts
@@ -5,45 +5,41 @@ import TianDiTuLayer from "app/utils/TianDiTuLayer";
 import SpatialReference from "esri/geometry/SpatialReference";
 import domConstruct from "dojo/dom-construct";
 import on from "dojo/on";
+import dom from "dojo/dom";
 
-const basemap = new Basemap({
-  baseLayers: [
-    new TianDiTuLayer({
-      urlTemplate: "http://t0.tianditu.com/vec_w/wmts",
-    } as TianDiTuLayer)
-  ]
+const mapWebMercator = new EsriMap({
+  basemap: new Basemap({
+    baseLayers: [
+      new TianDiTuLayer({
+        urlTemplate: "http://t0.tianditu.com/vec_w/wmts"
+      } as TianDiTuLayer)
+    ]
+  })
 });
 
-const map = new EsriMap({
-  basemap
-});
-
-const nextBasemap = new Basemap({
-  baseLayers: [
-    new TianDiTuLayer({
-      urlTemplate: "http://t0.tianditu.com/img_c/wmts",
-      spatialReference: SpatialReference.WGS84
-    } as TianDiTuLayer)
-  ]
-});
-
-const map2 = new EsriMap({
-  basemap: nextBasemap
+const mapWGS84 = new EsriMap({
+  basemap: new Basemap({
+    baseLayers: [
+      new TianDiTuLayer({
+        urlTemplate: "http://t0.tianditu.com/img_c/wmts",
+        spatialReference: SpatialReference.WGS84
+      } as TianDiTuLayer)
+    ]
+  })
 });
 
 const view = new SceneView({
-  map: map,
-  container: "viewDiv",
-  center: [120, 32],
-  zoom: 8
+  map: mapWebMercator,
+  container: "viewDiv"
 });
 
-const html = `<button id="btn" style="position: absolute; left: 50px; bottom: 50px">切换</button>`;
+const html = `<button id="btn" style="position: absolute; left: 50px; bottom: 50px">切换坐标</button>`;
 domConstruct.place(html, view.container, "last");
 
-const btn = document.getElementById("btn");
+const btn = dom.byId("btn");
+let i = 0;
 on(btn, "click", () => {
-  view.map = map2;
+  view.map = (++i % 2 === 0) ? mapWebMercator : mapWGS84;
 });
 
 view.ui.remove("attribution");
